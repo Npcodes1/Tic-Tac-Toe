865247da5bba55f9c48c6065a7ed4d0f6750d631
Nicole Payne <nicole1rock@gmail.com>
2023-05-14 16:03:56 -0500

    Testing X and O components

--- src/components/Square.test.js
+++ src/components/Square.test.js
@@ -1,10 +1,12 @@
 import { render, screen } from "@testing-library/react"
 import fireEvent from "@testing-library/user-event"
 import Square from "./Square.js"
+import "@testing-library/jest-dom/extend-expect";
+import { waitFor } from "@testing-library/react";
 
 describe("click on squares", () => {
     // Arrange- to transform a React component into the DOM node that the browser can display on screen. Set up conditions for the test.
-    it("should show an X or O upon clicking the square", () => {
+    it("should show an X upon clicking the square", () => {
         render(<Square />)
 
         // Act- where you actually execute the code you're testing.
@@ -15,6 +17,20 @@ describe("click on squares", () => {
         square = screen.findByRole("button", { name: "square" });
 
         //Assert- to verify the code being tested behaves as expected. 
-        expect(square).toHaveTextContent("X");
+        waitFor(() => expect("square").toHaveTextContent("X"));
+    });
+
+    it("should show an O upon clicking the square", () => {
+        render(<Square />)
+
+        // Act- where you actually execute the code you're testing.
+        let square = screen.findByRole("button", { name: "square" });
+        fireEvent.click(square);
+    
+        // Update our square variable with the new state after clicking.
+        square = screen.findByRole("button", { name: "square" });
+
+        //Assert- to verify the code being tested behaves as expected. 
+        waitFor(() => expect("square").toHaveTextContent("O"));
     });
 })
